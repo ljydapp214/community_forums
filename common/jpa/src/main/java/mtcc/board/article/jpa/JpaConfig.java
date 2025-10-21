287f98c8d688935bd45fee815a090ae3bd393bd3
jaeyounglee <ljyda214@gmail.com>
2025-10-21 23:46:52 +0900

    chg: Jpa 관련 config 스캔하도록 애노테이션 추가

--- common/jpa/src/main/java/mtcc/board/article/jpa/JpaConfig.java
+++ common/jpa/src/main/java/mtcc/board/article/jpa/JpaConfig.java
@@ -1,9 +1,13 @@
 package mtcc.board.article.jpa;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaAuditing
+@EntityScan(basePackages = "mtcc.board")
+@EnableJpaRepositories(basePackages = "mtcc.board")
 public class JpaConfig {
 }
